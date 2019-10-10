f82bb49d083d354488fb9dd359f83c752ce9f045
mgsgo <46468942+mgsgo@users.noreply.github.com>
2019-10-10 21:34:37 +0900

    example

---
+++ rev110/python_capture_code/cap3840x1080x30fps_display1920x1080x2.py
@@ -0,0 +1,21 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+while True:
+	ret, frame = capture.read()
+	image = frame.copy()
+	image_1 = image[0:1080,	0:1920]
+	image_2 = image[0:1080, 1920:3840]
+
+	cv2.imshow("video1", image_1)
+	cv2.imshow("video2", image_2)
+
+	if cv2.waitKey(1) > 0: break
+
+capture.release()
+cv2.destroyAllWindows()
---
+++ rev110/python_capture_code/cap3840x1080x30fps_display960x540x2.py
@@ -0,0 +1,21 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+while True:
+	ret, frame = capture.read()
+	image = frame.copy()
+	image_1 = image[0:1080,	0:1920]
+	image_2 = image[0:1080, 1920:3840]
+
+	image_1 = cv2.resize(image_1, (960, 540))
+	image_2 = cv2.resize(image_2, (960, 540))
+
+	cv2.imshow("video1", image_1)
+	cv2.imshow("video2", image_2)
+	if cv2.waitKey(1) > 0: break
+
+capture.release()
+cv2.destroyAllWindows()
